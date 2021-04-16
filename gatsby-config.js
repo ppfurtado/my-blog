60bfb9055c4c8c12760dc7ee55215aad8bb899b9
ppfurtado <furtado.pedropaulo@gmail.com>
2021-04-16 00:36:43 -0300

    add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `Pedro Paulo Furtado`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
